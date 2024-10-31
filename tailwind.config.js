7b3a627baf4fa5bf20d5d4664f39f58205d8d8e3
sunandasamanta <samantasunanda724@gmail.com>
2024-10-31 11:16:37 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#FF6464",
+            secondary: "#00A8CC",
+            light: "#EDF7FA",
+            grey: "#8695A4",
+            dark: "#21243D",
+            white: "#FFFFFF",
+            black: "#000000",
+        },
+        screens: {
+            sm: {
+                max: "640px",
+            },
+        },
+    },
+    plugins: [],
+};
